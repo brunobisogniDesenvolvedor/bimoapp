92699bba4168e08e34ad0d1f01135dd840b2ac0f
brunovirtuallight <36993341+brunovirtuallight@users.noreply.github.com>
2018-06-16 20:29:12 -0300

    parte login com a primeira tela com os 2 primeiros campos senha e login
    falta criar : nome   , número de telefone , cpf , endereço , email

--- app/src/main/java/br/com/bimo/bimoapp/MainActivity.java
+++ app/src/main/java/br/com/bimo/bimoapp/MainActivity.java
@@ -1,13 +1,45 @@
 package br.com.bimo.bimoapp;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnSignIn,btnRegister;
+
+    @Override
+    protected void attachBaseContext(Context newBase){
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
+
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
+        .setDefaultFontPath("fonts/Arkhip_font.ttf")
+        .setFontAttrId(R.attr.fontPath)
+        .build());
+        setContentView(R.layout.activity_main);
+    }
+         //int views
+
+
+    public Button getBtnRegister() {
+        return btnRegister=(Button)findViewById(R.id.btnRegister);
+    }
+
+    public Button getBtnSignIn() {
+        return btnSignIn=(Button)findViewById(R.id.btnSingIn);
     }
 }
+
